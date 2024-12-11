6d741717b72c537be8057b57b6abb2a44c6f0fd2
Jun Kai Zhang <junkaizhang8@gmail.com>
2024-12-10 23:55:31 -0500

    Implement typewriter effect in home section

--- js/script.mjs
+++ js/script.mjs
@@ -26,3 +26,75 @@ window.onscroll = () => {
   // Add faint shadow to the header when the user scrolls down
   header.classList.toggle("sticky", window.scrollY > 100);
 };
+
+// Wait for ms milliseconds
+const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+// Typewriter effect
+const createTypewriter = (
+  selector,
+  messages,
+  typeSpeed = 80,
+  deleteSpeed = 50,
+  delay = 2000,
+  loop = true
+) => {
+  if (!selector || selector === "") {
+    throw new Error("Selector is required");
+  }
+
+  if (!messages || messages.length === 0) {
+    throw new Error("Messages are required");
+  }
+
+  const element = document.querySelector(selector);
+  if (!element) {
+    throw new Error("Element not found");
+  }
+
+  let messageIndex = 0;
+
+  const typeMessage = async (message) => {
+    for (let i = 0; i < message.length; i++) {
+      element.textContent += message[i];
+      await wait(typeSpeed);
+    }
+  };
+
+  const deleteMessage = async () => {
+    for (let i = element.textContent.length; i >= 0; i--) {
+      element.textContent = element.textContent.slice(0, i);
+      await wait(deleteSpeed);
+    }
+  };
+
+  const typewriterLoop = async () => {
+    while (true) {
+      const currentMessage = messages[messageIndex];
+      await typeMessage(currentMessage);
+      element.classList.add("blink");
+      await wait(delay);
+      element.classList.remove("blink");
+      await deleteMessage();
+      messageIndex = (messageIndex + 1) % messages.length;
+
+      // Do not loop if the last message is reached and loop is false
+      if (!loop && messageIndex === 0) {
+        break;
+      }
+    }
+  };
+
+  typewriterLoop();
+};
+
+const typewriterMessages = [
+  "Software Developer",
+  "Full-Stack Developer",
+  "Web Developer",
+  "Game Developer",
+  "CS Student",
+  "Tech Enthusiast",
+];
+
+createTypewriter(".typewriter", typewriterMessages);
